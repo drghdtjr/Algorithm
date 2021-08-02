66b5401e3299e74b17ae38d6c06f9bc0138e1f94
drghdtjr <drghdtjr@naver.com>
2021-08-02 23:03:12 +0900

    210802

--- Baekjoon/step/5. 1차원 배열/5-6. 8958 OX퀴즈.py
+++ Baekjoon/step/5. 1차원 배열/5-6. 8958 OX퀴즈.py
@@ -1,6 +1,12 @@
 t=int(input())
-quiz_result=list(map(str,input()))
 for i in range (t):
-    o=1
-    x=0
-    if()
+    a=list(input())
+    sum_score=0
+    score=0
+    for j in  (a):
+        if (j=="O"):
+            score +=1
+        else:
+            score =0
+        sum_score+=score
+    print(sum_score)
